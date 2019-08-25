f14654f7a62b611d22869e94010f55052df354fc
Samantha Nelson <sam@brownboots.com>
2019-08-25 11:28:20 -0500

    tailwind is up and running

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//these are the vue dependencies - I added the router for the blog functionality
+
+import './assets/styles.css'
+//these are the tailwind base styles
 
 Vue.config.productionTip = false
 
