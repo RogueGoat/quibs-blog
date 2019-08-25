6d4c570b6fa422ed85be184bd3966713e4007de3
Samantha Nelson <sam@brownboots.com>
2019-08-25 11:34:30 -0500

    modified vue-cli generated components and views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Base></Base>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Base from '@/components/Base.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Base
   }
 }
 </script>
